7ac3135eb99805ae098eb786f7304b3d269ec711
Victor Johnson <johnsonsirv@gmail.com>
2023-10-24 13:29:09 +0100

    update package file

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-    testEnvironment: 'node'
-  };
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+  verbose: true,
+  testMatch: ['**/tests/**/?(*.)+(spec|test).[jt]s?(x)'],
+};
